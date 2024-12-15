7a86e0cebc0cfe30488a3a839a6b3952ea2a7d7a
Ganpati <ganpatinath07@gmail.com>
2024-12-15 13:14:38 +0530

    graph practice

--- GRAPH/practiceTwo.cpp
+++ GRAPH/practiceTwo.cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 class Graph {
     public:
-        unordered_map<int, list<int>> adjList;
-        void addEdge(int src, int dest, bool direction) {
+        unordered_map<int, list<pair<int, int>>> adjList;
+        void addEdge(int src, int dest, int weight, bool direction) {
             // direction == 0 --> Undirected Graph...
             // vice versa for Directed Graph...
-            adjList[src].push_back(dest);
+            adjList[src].push_back({dest, weight});
             if(direction == 0) {
-                adjList[dest].push_back(src);
+                adjList[dest].push_back({src, weight});
             }
         }
         void printADJ() {
             for(auto node : adjList) {
                 cout << node.first << " -> { ";
                 for(auto neigh : node.second) {
-                    cout << neigh << ", ";
+                    cout << "(" << neigh.first << ", " << neigh.second << "), ";
                 }
                 cout << "}";
                 cout << endl;
@@ -27,11 +27,11 @@ class Graph {
 };
 int main() {
     Graph g;
-    g.addEdge(0, 1, 1);
-    g.addEdge(0, 4, 1);
-    g.addEdge(2, 1, 1);
-    g.addEdge(2, 3, 1);
-    g.addEdge(3, 0, 1);
+    g.addEdge(0, 1, 6, 1);
+    g.addEdge(0, 4, 4, 1);
+    g.addEdge(2, 1, 5, 1);
+    g.addEdge(2, 3, 2, 1);
+    g.addEdge(3, 0, 7, 1);
     cout << "Printing ADJACENCY LIST..." << endl;
     g.printADJ();
     return 0;
