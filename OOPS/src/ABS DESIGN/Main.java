b6f423643e35ea143058d61ec08254f223c85fbf
Ganpati <ganpatinath07@gmail.com>
2024-12-27 20:44:16 +0530

    fundamentals

--- OOPS/src/ABS DESIGN/Main.java
+++ OOPS/src/ABS DESIGN/Main.java
@@ -1,37 +1,93 @@
+// // package ABS DESIGN;
+
+// abstract class Bird {
+
+//     abstract void eat();
+//     abstract void fly();
+
+// };
+
+// class Sparrow extends Bird {
+
+//     @Override
+//     void eat() {
+
+//         System.out.println("Sparrow is flying...");
+//     }
+
+//     @Override
+//     void fly() {
+
+//         System.out.println("Sparrow is eating...");
+//     }
+// };
+
+// class Eagle extends Bird {
+
+//     @Override
+//     void eat() {
+
+//         System.out.println("Eagle is flying...");
+//     }
+
+//     @Override
+//     void fly() {
+
+//         System.out.println("Eagle is eating...");
+//     }
+// };
+
+
+// public class Main {
+
+//     public static void doBird(Bird bird) {
+//         bird.eat();
+//         bird.fly();
+//     }
+
+//     public static void main (String[] args) {
+
+//         doBird(new Sparrow());
+//         doBird(new Eagle());
+
+//     }
+// }
+
+// using Interface...
+
 // package ABS DESIGN;
 
-abstract class Bird {
+interface Bird {
 
-    abstract void eat();
-    abstract void fly();
+    void fly();
+    void eat();
+}
 
-};
-
-class Sparrow extends Bird {
+class Sparrow implements Bird {
 
     @Override
-    void eat() {
+    public void eat() {
 
         System.out.println("Sparrow is flying...");
     }
 
     @Override
-    void fly() {
+    public void fly() {
 
         System.out.println("Sparrow is eating...");
     }
 };
 
-class Eagle extends Bird {
+class Eagle implements Bird {
 
     @Override
-    void eat() {
+    public void eat() {
 
         System.out.println("Eagle is flying...");
     }
 
     @Override
-    void fly() {
+    public void fly() {
 
         System.out.println("Eagle is eating...");
     }
@@ -40,14 +96,15 @@ class Eagle extends Bird {
 
 public class Main {
 
+    public static void doBird(Bird bird) {
+        bird.eat();
+        bird.fly();
+    }
+
     public static void main (String[] args) {
 
-        Bird bird = new Sparrow();
-        bird.eat();
-        bird.fly();
-
-        bird = new Eagle();
-        bird.eat();
-        bird.fly();
+        doBird(new Sparrow());
+        doBird(new Eagle());
+        
     }
 }
